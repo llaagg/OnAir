1bb4f9a6bebb597020da11811b7c55e4e8f797a9
Pawel Galezowski <pgalezowski@gmail.com>
2022-03-24 22:00:22 +0100

    added non packaed apps

--- OnAir.App/Logic/CameraStateProvider.cs
+++ OnAir.App/Logic/CameraStateProvider.cs
@@ -8,10 +8,11 @@ namespace OnAir.App.Logic
         public bool IsWebCamInUse()
         {
             using (var key = Registry.CurrentUser.OpenSubKey(
-                @"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam\NonPackaged"))
+                @"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam"))
             {
-                var listOfApplications = key.GetSubKeyNames().ToList();
-                foreach (var subKeyName in listOfApplications)
+                /// foreach app
+                var listOfApps = key.GetSubKeyNames().ToList();
+                foreach (var subKeyName in listOfApps)
                 {
                     using (var subKey = key.OpenSubKey(subKeyName))
                     {
@@ -25,14 +26,29 @@ namespace OnAir.App.Logic
                             // windows 10
                             if (endTime <= 0) return true;
 
-                            //// windows 11
-                            //var startTime = subKey.GetValue("LastUsedTimeStart") is long
-                            //    ? (long)subKey.GetValue("LastUsedTimeStart")
-                            //    : -1;
-                            //if (startTime > endTime)
-                            //{
-                            //    return true;
-                            //}
+                        }
+                    }
+                }
+            }
+            using (var key = Registry.CurrentUser.OpenSubKey(
+                @"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam\NonPackaged"))
+            {
+                var listOfApplications = key.GetSubKeyNames().ToList();
+                foreach (var subKeyName in listOfApplications)
+                {
+
+                    using (var subKey = key.OpenSubKey(subKeyName))
+                    {
+                        var lastUsed = subKey.GetValueNames().Contains("LastUsedTimeStop");
+                        if (lastUsed)
+                        {
+                            var endTime = subKey.GetValue("LastUsedTimeStop") is long
+                                ? (long)subKey.GetValue("LastUsedTimeStop")
+                                : -1;
+
+                            // windows 10
+                            if (endTime <= 0) return true;
+
                         }
                     }
                 }
